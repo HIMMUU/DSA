5b6908d9afc7bcef433b065bcc626df05075d287
HIMMUU <himeshg4@gmail.com>
2026-04-01 23:58:40 +0530

    Create Sum of Subarrays ranges.cpp

---
+++ Stack/Sum of Subarrays ranges.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    long long sumSubarrayMins(vector<int> arr) {
+        int n = arr.size();
+        vector<int> nse(n, 0);
+        vector<int> psee(n, 0);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[i] < arr[st.top()]) {
+                st.pop();
+            }
+            nse[i] = (st.empty()) ? n : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) {
+            st.pop();
+        }
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[i] <= arr[st.top()]) {
+                st.pop();
+            }
+            psee[i] = (st.empty()) ? -1 : st.top();
+            st.push(i);
+        }
+        long long total = 0;
+        for (int i = 0; i < n; i++) {
+            long long right = nse[i] - i;
+            long long left = i - psee[i];
+            total = (total + (right * left) * arr[i]);
+        }
+        return total;
+    }
+    long long sumSubarrayMaxs(vector<int> arr) {
+        int n = arr.size();
+        vector<int> nse(n, 0);
+        vector<int> psee(n, 0);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[i] > arr[st.top()]) {
+                st.pop();
+            }
+            nse[i] = (st.empty()) ? n : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) {
+            st.pop();
+        }
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[i] >= arr[st.top()]) {
+                st.pop();
+            }
+            psee[i] = (st.empty()) ? -1 : st.top();
+            st.push(i);
+        }
+        long long total = 0;
+        for (int i = 0; i < n; i++) {
+            long long right = nse[i] - i;
+            long long left = i - psee[i];
+            total = (total + (right * left) * arr[i]);
+        }
+        return total;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        long long ans = sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
+        return ans;
+    }
+};
