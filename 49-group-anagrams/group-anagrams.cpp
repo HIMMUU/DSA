b2a73827885dfe02b8c5619aa1b44192ee2ac522
HIMMUU <155185067+HIMMUU@users.noreply.github.com>
2026-07-25 00:55:14 +0530

    Time: 27 ms (22.77%) | Memory: 27.2 MB (22.40%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,11 +3,12 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res; 
      unordered_map<string , vector<int> > mpp ; 
-     vector<string> st = strs;
-     for( int i = 0 ;i < st.size() ; i++){
+     
+     for( int i = 0 ;i < strs.size() ; i++){
+        string key = strs[i];
+        sort(key.begin() , key.end());
+        mpp[key].push_back(i);
         
-        sort(st[i].begin() , st[i].end());
-        mpp[st[i]].push_back(i);
      }
      for( auto it : mpp){
         vector<int> temp = it.second ; 
