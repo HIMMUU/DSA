11f6650246dc2ec0aec6bdb599b4ac5c97fcfe01
HIMMUU <155185067+HIMMUU@users.noreply.github.com>
2026-07-25 01:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (71.29%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        long long sum = (1LL * n * (n + 1)) / 2;
+        long long sum = 0;
         long long arrsum = 0;
-        for (int i = 0; i < n; i++) { arrsum += nums[i]; }
+        for (int i = 0; i < n; i++) { 
+            sum += i;
+            arrsum += nums[i]; }
+            sum += n ; 
         return sum - arrsum;
     }
 };
