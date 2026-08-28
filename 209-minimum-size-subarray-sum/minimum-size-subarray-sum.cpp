7659671f6c9e6324b38b13b27e89813b3b9436e7
HIMMUU <155185067+HIMMUU@users.noreply.github.com>
2026-08-29 00:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (74.31%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,26 +1,3 @@
-// class Solution {
-// public:
-//     int minSubArrayLen(int target, vector<int>& nums) {
-//         int n = nums.size();
-//         int l = 0, r = 0;
-//         int sum = 0;
-//         int ans = INT_MAX;
-//         while (r < n) {
-//             sum += nums[r];
-
-//             while (sum >= target) {
-//                 sum -= nums[l];
-//                 l++;
-//                 if( sum >= target)
-//                 ans = min(ans , r-l+1);
-            
-                
-//             }
-//             r++;
-//         }
-//         return (ans != INT_MAX) ? ans : 0;
-//     }
-// };
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
@@ -28,20 +5,20 @@ public:
         int l = 0, r = 0;
         int sum = 0;
         int ans = INT_MAX;
-
         while (r < n) {
             sum += nums[r];
 
             while (sum >= target) {
-                ans = min(ans, r - l + 1);
-
+             ans = min(ans , r-l+1);
                 sum -= nums[l];
                 l++;
+               
+              
+            
+                
             }
-
             r++;
         }
-
         return (ans != INT_MAX) ? ans : 0;
     }
 };
